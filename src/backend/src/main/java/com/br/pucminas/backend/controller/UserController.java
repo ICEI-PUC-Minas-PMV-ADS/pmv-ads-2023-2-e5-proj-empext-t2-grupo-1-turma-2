5036f95295ff4ffaa73bc903d0a26e734b55be32
Pedro Daniel Jardim <pedrodanieljardim@gmail.com>
2023-10-07 14:38:22 -0300

    fix: correção no pom

--- src/backend/src/main/java/com/br/pucminas/backend/controller/UserController.java
+++ src/backend/src/main/java/com/br/pucminas/backend/controller/UserController.java
@@ -7,10 +7,7 @@ import com.br.pucminas.backend.model.usercase.UserForm;
 import com.br.pucminas.backend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
