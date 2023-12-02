9aefa4ce2dc1b16490b80519b03d5336bc9d989f
Pedro Daniel Jardim <pedrodanieljardim@gmail.com>
2023-12-02 14:45:44 -0300

    feat: Ajustes no backend

--- src/backend/src/main/java/com/br/pucminas/backend/repository/EmailRepository.java
+++ src/backend/src/main/java/com/br/pucminas/backend/repository/EmailRepository.java
@@ -2,8 +2,10 @@ package com.br.pucminas.backend.repository;
 
 import com.br.pucminas.backend.domain.entity.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface EmailRepository extends JpaRepository<Email, UUID> {
 }
